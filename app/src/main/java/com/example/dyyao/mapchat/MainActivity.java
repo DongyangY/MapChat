58a39abd2ccfd3f80aaca9605b640f2468ef9e71
Lulu Zhao <lulu.zhao1230@gmail.com>
2015-11-15 16:58:28 -0500

    lulu's layout1

--- app/src/main/java/com/example/dyyao/mapchat/MainActivity.java
+++ app/src/main/java/com/example/dyyao/mapchat/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.dyyao.mapchat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bTry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bTry = (Button) findViewById(R.id.btn_try);
+        bTry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MapChat.class));
+
+            }
+
+
+        });
+
     }
 }
+
