a9b52905227b22e1643e64386ef1487cc7b04c4e
HuaDeng <harolddeng@gmail.com>
2015-11-21 15:38:37 -0500

    huadeng

--- app/src/main/java/com/example/dyyao/mapchat/AddFriend.java
+++ app/src/main/java/com/example/dyyao/mapchat/AddFriend.java
@@ -3,9 +3,13 @@ package com.example.dyyao.mapchat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by luluzhao on 11/15/15.
@@ -13,22 +17,27 @@ import android.widget.EditText;
 public class AddFriend extends AppCompatActivity {
     EditText userID;
     Button bAddFriend;
+    public Queue<String> mFriendInfoBuffer;
+    private static final String TAG = "AddFriend";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ClientTaskWR.addFriend = this;
         setContentView(R.layout.activity_addfriend);
         bAddFriend = (Button) findViewById(R.id.btn_add);
         userID = (EditText) findViewById(R.id.et_userid);
+
+        //mFriendInfoBuffer = new LinkedList<>();
+
         bAddFriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(AddFriend.this, friendList.class));
-
+                Log.d(TAG, "at");
+                Login.mLogCommandBuffer.add("add_friend:" + userID.getText().toString());
+                Log.d(TAG, "buffer size " + String.valueOf(Login.mLogCommandBuffer.size()));
+                //startActivity(new Intent(AddFriend.this, friendList.class));
             }
-
-
         });
 
     }
-
 }
