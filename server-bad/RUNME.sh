40753c0dfb364e4364a0cf735e778d51370131a8
DongyangY <dongyang.yao@rutgers.edu>
2016-09-07 09:10:19 +0800

    delete bad server

--- server-bad/RUNME.sh
+++
@@ -1,2 +0,0 @@
-javac MapChatServer.java ServerThread.java ClientThread_push.java ClientThread_request.java ClientThread_transfer.java Group.java Link.java Push.java
-java -cp .:Library/mysql-connector-java-5.1.34-bin.jar MapChatServer
